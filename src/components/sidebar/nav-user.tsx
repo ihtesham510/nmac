740efee5ece90e743548e887bd53c0004dddc514
ihtesham510 <ihteshamulhaq510@gmail.com>
2025-05-31 17:42:36 +0500

    fixed seperator line in nav-user

--- src/components/sidebar/nav-user.tsx
+++ src/components/sidebar/nav-user.tsx
@@ -2,6 +2,7 @@ import { CreditCardIcon, LogOutIcon } from 'lucide-react'
 import { Progress } from '@/components/ui/progress'
 import { Separator } from '@/components/ui/separator'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import React from 'react'
 import {
 	SidebarMenu,
 	SidebarMenuButton,
@@ -43,32 +44,34 @@ export function NavUser({ user, credits, ...props }: NavUserProps) {
 					</div>
 				</div>
 			</SidebarMenuItem>
-			<Separator className='my-1' />
 			{credits && (
-				<SidebarMenuItem className='flex flex-col space-y-2 my-1 mx-1'>
-					<div className='flex gap-2 items-center'>
-						<CreditCardIcon className='size-4' />
-						<p className='font-medium text-sm'>Credits</p>
-					</div>
-					<div className='flex flex-col gap-2'>
-						<div className='flex flex-col py-2 gap-2'>
-							<span className='font-medium flex justify-between items-center text-sm'>
-								<p>Total :</p>
-								<p> {credits?.total}</p>
-							</span>
-							<span className='font-medium flex justify-between items-center text-sm'>
-								<p>Remaining :</p>
-								<NumberFlow value={Math.round(credits.remaining)} />
-							</span>
+				<React.Fragment>
+					<Separator className='my-1' />
+					<SidebarMenuItem className='flex flex-col space-y-2 my-1 mx-1'>
+						<div className='flex gap-2 items-center'>
+							<CreditCardIcon className='size-4' />
+							<p className='font-medium text-sm'>Credits</p>
 						</div>
-						<Progress
-							value={100 - percentUsed}
-							max={100}
-							destructive={100 - percentUsed < 20}
-							className='w-auto h-1.5'
-						/>
-					</div>
-				</SidebarMenuItem>
+						<div className='flex flex-col gap-2'>
+							<div className='flex flex-col py-2 gap-2'>
+								<span className='font-medium flex justify-between items-center text-sm'>
+									<p>Total :</p>
+									<p> {credits?.total}</p>
+								</span>
+								<span className='font-medium flex justify-between items-center text-sm'>
+									<p>Remaining :</p>
+									<NumberFlow value={Math.round(credits.remaining)} />
+								</span>
+							</div>
+							<Progress
+								value={100 - percentUsed}
+								max={100}
+								destructive={100 - percentUsed < 20}
+								className='w-auto h-1.5'
+							/>
+						</div>
+					</SidebarMenuItem>
+				</React.Fragment>
 			)}
 			<Separator className='my-1 mb-2' />
 			<SidebarMenuItem>
