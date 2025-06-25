fb7b4d6bbc91306b0df48bef176753334de2de96
ihtesham510 <ihteshamulhaq510@gmail.com>
2025-06-25 15:08:29 +0500

    formatted the files

--- src/routes/dashboard/404.tsx
+++ src/routes/dashboard/404.tsx
@@ -1,9 +1,9 @@
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/dashboard/404')({
-  component: RouteComponent,
+	component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/dashboard/404"!</div>
+	return <div>Hello "/dashboard/404"!</div>
 }
--- src/routes/dashboard/505.tsx
+++ src/routes/dashboard/505.tsx
@@ -1,9 +1,9 @@
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/dashboard/505')({
-  component: RouteComponent,
+	component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/dashboard/505"!</div>
+	return <div>Hello "/dashboard/505"!</div>
 }
