e54fb6c052caac68c83749fc9ff11ca8c3e0c8ef
ihtesham510 <ihteshamulhaq510@gmail.com>
2025-07-10 02:41:41 +0500

    removed auth in webhook

--- convex/http.ts
+++ convex/http.ts
@@ -9,21 +9,14 @@ http.route({
 	method: 'POST',
 	handler: httpAction(async (ctx, req) => {
 		console.log('webhook body', req.json())
-		const header = req.headers.get('ElevenLabs-Signature')
-		const timestamp = header?.split(',').find(e => e.startsWith('t='))
-		const signature = header?.split(',').find(e => e.startsWith('v0='))
-		if (timestamp && signature) {
-			const body = await req.json()
-			const agent_id = body.data.agent_id
-			const cost = body.data.metadata.cost
-			await ctx.runMutation(internal.internals.deduct, {
-				cost,
-				agent_id,
-			})
-			return new Response(null, { status: 200 })
-		}
-
-		return new Response(null, { status: 401 })
+		const body = await req.json()
+		const agent_id = body.data.agent_id
+		const cost = body.data.metadata.cost
+		await ctx.runMutation(internal.internals.deduct, {
+			cost,
+			agent_id,
+		})
+		return new Response(null, { status: 200 })
 	}),
 })
 
